9c384ba39d6c9e2efdd28c3fabc0487bbd7b9053
Michael Wilson <pianoplayer868@hotmail.com>
2024-05-26 21:53:29 -0700

    Odd behaviour... sometimes the use of useAppDispatch works within the HOC method of the class wrapper and other times it doesn't. After looking through documentation hooks should not be in callback wrappers and therefore I have reverted the logic back to a functional component style

--- src/components/CheckboxPicker/CheckboxPicker.tsx
+++ src/components/CheckboxPicker/CheckboxPicker.tsx
@@ -8,30 +8,12 @@ import { CARD_COLOR, DATASET_TYPES } from '../../utils/MagicConstants';
 import { AppDispatch } from '../../redux/Store';
 
 export interface ICheckboxPicker {
-    dispatch: AppDispatch;
     options: string[][];
 }
 
-function CheckBoxPickerWrapper(WrappedComponent: any) {
-    return function(props: any) {
-        const dispatch = useAppDispatch();
-        return (
-            <WrappedComponent dispatch={dispatch} {...props} />
-        )
-    }
-}
-
-export class CheckboxPicker extends React.Component<ICheckboxPicker> {
-    private dispatch;
-    private options;
-
-    constructor(props: ICheckboxPicker) {
-        super(props);
-        this.options = props.options;
-        this.dispatch = props.dispatch;
-    }
-
-    private handleChecked(checkbox: HTMLInputElement) {
+export const CheckboxPicker = ({options}: ICheckboxPicker) => {
+    const dispatch = useAppDispatch();
+    const handleChecked = (checkbox: HTMLInputElement) => {
         // if single colors are selected, disable all other options
         const selectedColor = checkbox.value;
 
@@ -52,34 +34,32 @@ export class CheckboxPicker extends React.Component<ICheckboxPicker> {
         }
 
         if (checkbox.checked) {
-            this.dispatch(addColor(selectedColor));
+            dispatch(addColor(selectedColor));
         } else {
-            this.dispatch(removeColor(selectedColor));
+            dispatch(removeColor(selectedColor));
         }
     }
 
-    public render() {
-        return (
-            <div role='radiogroup'>
-                {this.options.map(option => (
-                    <label key={option[0]} className='label-container'>{option[0]}
-                        <input 
-                            type='checkbox'
-                            data-color-type={
-                                isSingleColor(option[1]) ?
-                                    CARD_COLOR.single :
-                                    (isGuild(option[1]) ?
-                                        CARD_COLOR.guild :
-                                        CARD_COLOR.shard)
-                            }
-                            onClick={(checkbox) => this.handleChecked(checkbox.currentTarget)}
-                            value={option[1]}/>
-                        <span className='checkmark'></span>
-                    </label>
-                ))}
-            </div>
-        );
-    }
+    return (
+        <div role='radiogroup'>
+            {options.map(option => (
+                <label key={option[0]} className='label-container'>{option[0]}
+                    <input 
+                        type='checkbox'
+                        data-color-type={
+                            isSingleColor(option[1]) ?
+                                CARD_COLOR.single :
+                                (isGuild(option[1]) ?
+                                    CARD_COLOR.guild :
+                                    CARD_COLOR.shard)
+                        }
+                        onClick={(checkbox) => handleChecked(checkbox.currentTarget)}
+                        value={option[1]}/>
+                    <span className='checkmark'></span>
+                </label>
+            ))}
+        </div>
+    );
 }
 
-export default CheckBoxPickerWrapper(CheckboxPicker);
+export default CheckboxPicker;
